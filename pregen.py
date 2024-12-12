f5aa43b3c6fb9a05743e3426e17d5c065c5e12d0
Z4TE <kayakun426@gmail.com>
2024-12-12 14:25:09 +0900

    Initial commit.

---
+++ pregen.py
@@ -0,0 +1,18 @@
+from mcrcon import MCRcon
+import tqdm
+
+server_address  = "localhost"
+server_pass     = "specify a good password"
+server_port     = "25575"
+
+radius = 128; # in chunks
+
+range = radius << 4
+start = range * -1 + 1
+
+for x in tqdm.tqdm(range(start, range, 16)):
+    for y in tqdm.tqdm(range(start, range, 16), leave = False):
+        # generate chunks
+        with MCRcon(server_address, server_pass, server_port) as mcr:
+            mcr.command(f"forceload add {x} {y}")
+            mcr.command(f"forceload remove {x} {y}")
